832ab836479cd95175a04f3e4faf376ae217968e
Martin Schaller <dh1df@darc.de>
2021-02-25 16:17:09 +0100

    Added support for running custom scripts

--- init.lua
+++ init.lua
@@ -1,8 +1,36 @@
 -- dofile"SSD1306.lua"
 dofile"SSD1327-waveshare-128x128.lua"
+
+function run_file(file)
+    print("Starting",file)
+    dofile(file)
+end
+
+function run_prefix(prefix)
+    local files={}
+    for key,value in pairs(file.list()) do
+        if (key:match(prefix.."%d%d.*%.lua")) then
+	    table.insert(files,key) 
+        end
+    end
+    table.sort(files)
+    for i,value in ipairs(files) do
+        run_file(value)
+    end
+end
+
+function start()
+    run_prefix("S")
+    run_file("is.lua")
+end
+
+function kill()
+    run_prefix("K")
+end
+
 boottimer = tmr.create()
 print("Booting in 5 seconds, enter stop() to cancel")
-boottimer:register(5000, tmr.ALARM_SINGLE, function() print("Starting is.lua now. "); require"is"; end)
+boottimer:register(5000, tmr.ALARM_SINGLE, start)
 boottimer:start()
 
 function stop()
