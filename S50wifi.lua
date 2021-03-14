b46d54114947df777db5f756f67da6ff13aa2659
Martin Schaller <dh1df@darc.de>
2021-03-14 14:45:16 +0100

    Cleaned up wifi configuration

--- S50wifi.lua
+++ S50wifi.lua
@@ -3,91 +3,42 @@
 
 print("WiFi Mode: ", wlanmode)
 
-if wlanmode == 3 then
-
-print("Starting WiFi in STATIONAP mode")
-
--- run WiFi AP and connect to WiFi access point
-
-wifi.mode(wifi.STATIONAP, true)
-
-wifi.sta.on("connected", function() print("connected") end)
--- wifi.sta.on("got_ip", function(event, info) print("got ip "..info.ip) localstaip = info.ip end)
-wifi.sta.on("got_ip", function(event, info)  print("got ip "..info.ip) localstaip = info.ip end)
-
-wifi.ap.on("start")
-wifi.ap.on("sta_connected", function(event, info) print("Station connected:  "..info.mac ) end)
-
--- mandatory to start wifi after reset
-wifi.start()
-
--- wifi.sta.sethostname is broken.
--- wifi.sta.sethostname("FF-ESP32")
-
-cfg={}
-
-cfg.ssid=ap_ssid
-cfg.pwd=ap_pwd
-wifi.ap.config(cfg)
-cfg={}
-cfg.ip=ap_ip
-cfg.netmask=ap_netmask
-cfg.gateway=ap_gateway
--- Possible conflict, if station is on a different channel.
--- cfg.channel=ap_channel
-cfg.dns=ap_dns
-
-wifi.ap.setip(cfg)
-
-
-wifi.sta.config({ssid=sta_ssid, pwd=sta_pwd, auto=true}, true)
-
+local function config_sta()
+	wifi.sta.on("connected", function() print("sta connected") end)
+	wifi.sta.on("got_ip", function(event, info)  print("sta got ip "..info.ip) localstaip = info.ip end)
+	wifi.sta.config({ssid=sta_ssid, pwd=sta_pwd, auto=true}, true)
+	if (sta_hostname ~= nil and sta_hostname ~= '') then
+		wifi.sta.sethostname(sta_hostname)
+	end
 end
 
-if wlanmode == 2  then
-
--- Run as WiFi access point
-
-wifi.mode(wifi.SOFTAP, true)
-
-wifi.ap.on("start")
-wifi.ap.on("sta_connected", function(event, info) print("Station connected:  "..info.mac ) end)
-
--- mandatory to start wifi after reset
-wifi.start()
-cfg={}
-
-cfg.ssid=ap_ssid
-cfg.pwd=ap_pwd
-wifi.ap.config(cfg)
-cfg={}
-cfg.ip=ap_ip
-cfg.netmask=ap_netmask
-cfg.gateway=ap_gateway
-cfg.channel=ap_channel
-cfg.dns=ap_dns
-
-wifi.ap.setip(cfg)
-
+local function config_ap(channel)
+	wifi.ap.on("start")
+	wifi.ap.on("sta_connected", function(event, info) print("Station connected:  "..info.mac ) end)
+	wifi.ap.config({ssid=ap_ssid, pwd=ap_pwd})
+	wifi.ap.setip({ip=ap_id, netmask=ap_netmask, gateway=ap_gateway, channel=channel, dns=ap_dns})
+	if (ap_hostname ~= nil and ap_hostname ~= '') then
+		wifi.ap.sethostname(ap_hostname)
+	end
 end
-
 if wlanmode == 1 then
-
--- Run as WiFi client
-wifi.mode(wifi.STATION, true)
-
-wifi.sta.on("connected", function() print("connected") end)
---wifi.sta.on("got_ip", function(event, info) print("got ip "..info.ip) end)
-wifi.sta.on("got_ip", function(event, info) print("got ip "..info.ip) localstaip = info.ip end)
-
--- wifi.sta.sethostname is broken.
---wifi.sta.sethostname("FF-ESP32")
-
--- mandatory to start wifi after reset
+	wifi.mode(wifi.STATION, true)
+end
+if wlanmode == 2 then
+	wifi.mode(wifi.SOFTAP, true)
+end
+if wlanmode == 3 then
+	wifi.mode(wifi.STATIONAP, true)
+end
 wifi.start()
-
-wifi.sta.config({ssid=sta_ssid, pwd=sta_pwd, auto=true}, true)
-
+if wlanmode == 1 or wlanmode == 3 then
+	config_sta()
+end
+if wlanmode == 2 then
+	config_ap(ap_channel)
+end
+if wlanmode == 3 then
+	config_ap(nil)
 end
 
 uplinktimer = tmr.create()
