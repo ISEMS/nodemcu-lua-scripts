ca894d2243ae896534c23e225b4590e855191acb
elektra42 <onelektra@gmx.net>
2021-03-16 13:18:16 +0100

    Improved ADC accuracy for V_in/V_oc and V_out. Improved MPPT-PWM accuracy. Updated board data.

--- board.lua
+++ board.lua
@@ -1,14 +1,23 @@
 -- Calibration and board data
 
+-- MPP range of FF-OpenMPPT-ESP32 v1.0
+--      Vmpp_max = 23.8 
+--      Vmpp_min = 14.45
+
+
+-- MPP range of FF-OpenMPPT-ESP32 v1.1
+--       Vmpp_max = 27.2
+--       Vmpp_min = 13.25
+
 -- MPP range of FF-OpenMPPT-ESP32 v1.2
 
-    Vmpp_max = 27.2
-    Vmpp_min = 13.25
+    Vmpp_max = 25.15
+    Vmpp_min = 12.86
 
 -- The Vcc rail of the ESP32 is the Voltage 
 -- reference for the temperature sensor 
 
-    Vcc = 3.07
+    Vcc = 3.03
 
     hardware_version = "1.2"
     firmware_type = "ESP_1B"
