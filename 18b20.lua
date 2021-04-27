f9a58a74217be2dbbb466858912b9d9cea64fb4c
elektra42 <onelektra@gmx.net>
2021-04-27 14:06:47 +0200

    Quicker i2c scan, changed GPIO port of i2c interface to no. 22

--- 18b20.lua
+++ 18b20.lua
@@ -1,59 +1,54 @@
--- 18b20 Example
--- local owtimer = tmr.create()
-pin = 21
+-- ds18b20 connected to GPIO22
+ 
+local pin = 22
+local addr = nil 
 ow.setup(pin)
-count = 0
+local count = 0
 repeat
   count = count + 1
   addr = ow.reset_search(pin)
   addr = ow.search(pin)
 until (addr ~= nil) or (count > 3)
 if addr == nil then
-  print("No more addresses.")
+  printv(4,"18b20: No more addresses.")
 else
-  print(addr:byte(1,8))
-  crc = ow.crc8(string.sub(addr,1,7))
+  printv(4,addr:byte(1,8))
+  local crc = ow.crc8(string.sub(addr,1,7))
   if crc == addr:byte(8) then
     if (addr:byte(1) == 0x10) or (addr:byte(1) == 0x28) then
-      print("Device is a DS18S20 family device.")
---        repeat
+      printv(4,"Device is a DS18S20 family device.")
           ow.reset(pin)
           ow.select(pin, addr)
           ow.write(pin, 0x44, 1)
-          -- tmr.delay(1000000)
-          
-          --owtimer:register(10, tmr.ALARM_SINGLE, function (t) -- print("expired") end)
-          present = ow.reset(pin)
+          local present = ow.reset(pin)
           ow.select(pin, addr)
           ow.write(pin,0xBE,1)
-          print("P="..present)
-          data = nil
-          data = string.char(ow.read(pin))
+          printv(4,"18b20: P="..present)
+          local data = nil
+          local data = string.char(ow.read(pin))
+	  local increment 		
           for increment = 1, 8 do
             data = data .. string.char(ow.read(pin))
           end
-          print(data:byte(1,9))
+
+          printv(4,"18b20:",data:byte(1,9))
           crc = ow.crc8(string.sub(data,1,8))
-          print("CRC="..crc)
+          printv(4,"18b20: CRC="..crc)
+          
           if crc == data:byte(9) then
-             tbits = (data:byte(1) + data:byte(2) * 256) * 625
+             local tbits = (data:byte(1) + data:byte(2) * 256) * 625
              ow_temp1 = tbits / 10000
             if ow_temp1 > 200 then ow_temp1 = ow_temp1 - 4095 
           end
-             --t2 = t % 10000
-             --print("Temperature="..t1.."."..t2.." Centigrade")
-             print("Temperature = " ..ow_temp1 .. " Celsius")
-         end
- --       until false
-            --              end)
-            -- owtimer:start()    
+             printv(2,"Temp_i2c = " ..ow_temp1 .. " Celsius\n")
+         end    
     else
-      print("Device family is not recognized.")
+      printv(4,"18b20: Device family is not recognized.\n")
           end
   else
-    print("CRC is not valid!")
-
+    printv(4,"18b20: CRC is not valid!\n")
+    
+  end
 
 end
 
-end
